e8a61e2478b194fe521b2f32bc6a4e05fd9cbc00
Fadde <obeyfranso@gmail.com>
2023-09-22 02:10:49 +0200

    feat: applies image to wallpaper, can alter the fitting as well

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -32,9 +32,11 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.showAPictureButton = new System.Windows.Forms.Button();
             this.SetWallpaperButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.WallpaperStyleSelector = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -42,9 +44,9 @@
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -53,17 +55,19 @@
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.showAPictureButton);
             this.flowLayoutPanel1.Controls.Add(this.SetWallpaperButton);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.WallpaperStyleSelector);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 408);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 507);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(794, 39);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(778, 51);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
             // showAPictureButton
@@ -88,6 +92,42 @@
             this.SetWallpaperButton.UseVisualStyleBackColor = true;
             this.SetWallpaperButton.Click += new System.EventHandler(this.SetWallpaperButton_Click);
             // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose a fit for your image";
+            // 
+            // WallpaperStyleSelector
+            // 
+            this.WallpaperStyleSelector.FormattingEnabled = true;
+            this.WallpaperStyleSelector.Items.AddRange(new object[] {
+            "Center",
+            "Fit",
+            "Stretch",
+            "Tile",
+            "Span"});
+            this.WallpaperStyleSelector.Location = new System.Drawing.Point(335, 3);
+            this.WallpaperStyleSelector.Name = "WallpaperStyleSelector";
+            this.WallpaperStyleSelector.Size = new System.Drawing.Size(121, 21);
+            this.WallpaperStyleSelector.TabIndex = 2;
+            this.WallpaperStyleSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.WallpaperStyleSelector.SelectedIndex = 0;           
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(778, 498);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
@@ -95,28 +135,22 @@
     "s (*.*)|*.*";
             this.openFileDialog1.Title = "Select a picture file";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(794, 399);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainWindow";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -128,6 +162,8 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button showAPictureButton;
         private System.Windows.Forms.Button SetWallpaperButton;
+        private System.Windows.Forms.ComboBox WallpaperStyleSelector;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -1,14 +1,35 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace UnsplashDesktopSetter
 {
     public partial class MainWindow : Form
     {
+        Image _CurrentImage = null;
+        string _FullPathToImage = null;
+        private const int SPI_SETDESKWALLPAPER = 20;
+        private const int SPIF_UPDATEINIFILE = 0x1;
+        private const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum WallpaperStyle : int
+        {
+            Tile,
+            Center,
+            Stretch,
+            Fit,
+            Span
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            pictureBox1.Image = _CurrentImage;
         }
 
         private void ShowAPictureButton_Click(object sender, EventArgs e)
@@ -28,11 +49,55 @@ namespace UnsplashDesktopSetter
             }
 
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            _FullPathToImage = pictureBox1.ImageLocation;
         }
 
         private void SetWallpaperButton_Click(object sender, EventArgs e)
         {
-            // TODO: set wallpaper logic
+            if (_FullPathToImage == null)
+            {
+                return;
+            }
+
+            WallpaperStyle style = (WallpaperStyle)Enum.Parse(
+                typeof(WallpaperStyle),
+                WallpaperStyleSelector.SelectedItem.ToString());
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == WallpaperStyle.Fit)
+            {
+                key.SetValue(@"WallpaperStyle", 6.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == WallpaperStyle.Span)
+            {
+                key.SetValue(@"WallpaperStyle", 22.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == WallpaperStyle.Stretch)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == WallpaperStyle.Center)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == WallpaperStyle.Tile)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                _FullPathToImage,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
         }
     }
 }
